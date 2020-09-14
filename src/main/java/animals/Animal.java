932f2366e9c62fb07aa58b46b532a70e0d40da88
Vin-Cento <vincentliu362@gmail.com>
2020-09-13 21:44:27 -0600

    remove spacing

--- src/main/java/animals/Animal.java
+++ src/main/java/animals/Animal.java
@@ -14,27 +14,20 @@ public abstract class Animal {
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public int getAge() {
         return age;
     }
-
     public void setAge(int age) {
         this.age = age;
     }
 
     // abstract methods
     abstract public void wakeUp();
-
     abstract public void makeNoise();
-
     abstract public void eat();
-
     abstract public void roam();
-
     abstract public void sleep();
 }
