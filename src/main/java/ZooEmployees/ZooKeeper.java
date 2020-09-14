becf66c62e9edcd529d3d9cb08f742e170fce638
Vin-Cento <vincentliu362@gmail.com>
2020-09-13 21:02:15 -0600

    add function body

--- src/main/java/ZooEmployees/ZooKeeper.java
+++ src/main/java/ZooEmployees/ZooKeeper.java
@@ -12,26 +12,36 @@ public class ZooKeeper extends ZooEmployees {
 
     @Override
     public void wakeUpAnimal(Collection<Animal> ZooAnimals) {
-        
+        for(Animal creature : ZooAnimals){
+            creature.wakeUp();
+        }
     }
 
     @Override
     public void makeNoiseAnimal(Collection<Animal> ZooAnimals) {
-
+        for(Animal creature : ZooAnimals){
+            creature.makeNoise();
+        }
     }
 
     @Override
     public void eatAnimal(Collection<Animal> ZooAnimals) {
-
+        for(Animal creature : ZooAnimals){
+            creature.eat();
+        }
     }
 
     @Override
     public void roamAnimal(Collection<Animal> ZooAnimals) {
-
+        for(Animal creature : ZooAnimals){
+            creature.roam();
+        }
     }
 
     @Override
     public void sleepAnimal(Collection<Animal> ZooAnimals) {
-
+        for(Animal creature : ZooAnimals){
+            creature.sleep();
+        }
     }
 }
