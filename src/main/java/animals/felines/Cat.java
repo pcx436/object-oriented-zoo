b23f57a4a5e9d776c50f319d49c3dd5cfad4c9a3
Vin-Cento <vincentliu362@gmail.com>
2020-09-13 21:46:32 -0600

    init cat extended from feline

---
+++ src/main/java/animals/felines/Cat.java
@@ -0,0 +1,27 @@
+package animals.felines;
+
+public class Cat extends Feline{
+    public Cat(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeNoise() {
+
+    }
+
+    @Override
+    public void eat() {
+
+    }
+
+    @Override
+    public void roam() {
+
+    }
+
+    @Override
+    public void sleep() {
+
+    }
+}
