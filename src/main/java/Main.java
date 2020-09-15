ad94ea49b7745817ad942aa7dbdd77cc91894374
Jacob Malcy <pcx436@yahoo.com>
2020-09-15 10:38:59 -0600

    Adding ArrayList and collection imports

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
 import animals.Animal;
 import  animals.felines.Cat;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class Main {
     public static void  main(String[] args) {
         Cat fred=new Cat("Fred",8);
