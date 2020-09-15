9549967079deebeb0d710050fa05289a4512181c
Vin-Cento <vincentliu362@gmail.com>
2020-09-14 20:40:02 -0600

    defined sleep, eat, and wakeup methods

--- src/main/java/animals/felines/Feline.java
+++ src/main/java/animals/felines/Feline.java
@@ -10,7 +10,15 @@ public abstract class Feline extends Animal {
 
     // abstract methods
     public void wakeUp(){
-        System.out.println(this.getName() + ", the " + this.getClass().getSimpleName() + ", woke up.");
+        System.out.println(this.getName() + ", the " + this.getClass().getSimpleName().toLowerCase() + ", woke up.");
+    }
+
+    public void sleep(){
+        System.out.println(this.getName() + ", the " + this.getClass().getSimpleName().toLowerCase() + ", went to sleep.");
+    }
+
+    public void eat(){
+        System.out.println(this.getName() + ", the " + this.getClass().getSimpleName().toLowerCase() + ", was fed.");
     }
     abstract public void makeNoise();
 /*
