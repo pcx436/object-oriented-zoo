6fe40c5c87ee5910383c891d084e9a38bf57e76f
Jacob Malcy <pcx436@yahoo.com>
2020-09-15 15:50:56 -0600

    Get the number of days for our loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,7 @@ import zooEmployees.ZooKeeper;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Scanner;
 
 public class Main {
     public static void  main(String[] args) {
@@ -20,6 +21,10 @@ public class Main {
         // creating ZooKeeper
         ZooKeeper gronk = new ZooKeeper("Gronk", 9000, animals);
 
+        // get user input days
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please enter the number of days: ");
+        int numDays = scan.nextInt();
         gronk.eatAnimal();
 
     }
