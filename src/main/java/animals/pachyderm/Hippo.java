abe174b504b97cb47f96b17738de37653b468244
Vin-Cento <vincentliu362@gmail.com>
2020-09-15 16:17:23 -0600

    add special case for roam

--- src/main/java/animals/pachyderm/Hippo.java
+++ src/main/java/animals/pachyderm/Hippo.java
@@ -1,7 +1,21 @@
 package animals.pachyderm;
 
+import java.util.Random;
+
 public class Hippo extends Pachyderm {
     public Hippo(String name, int age) {
         super(name, age);
+    }
+
+    Random rand = new Random();
+    int num = rand.nextInt(4);
+
+    @Override
+    public void roam() {
+        if(num == 0){
+            System.out.println(this.getName() + ", the " + this.getClass().getSimpleName().toLowerCase() + ", charged at Zoo Keeper.");
+        } else{
+            System.out.println(this.getName() + ", the " + this.getClass().getSimpleName().toLowerCase() + ", roamed around.");
+        }
     }
 }
