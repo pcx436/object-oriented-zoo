f8cfbe1b0975989242cb2db5d2cb703c62d7f6fc
Vin-Cento <vincentliu362@gmail.com>
2020-09-15 15:48:35 -0600

    add the special case for dog.roam

--- src/main/java/animals/canine/Dog.java
+++ src/main/java/animals/canine/Dog.java
@@ -1,12 +1,26 @@
 package animals.canine;
 
+import java.util.Random;
+
 public class Dog extends Canine{
     public Dog(String name, int age) {
         super(name, age);
     }
 
+    Random rand = new Random();
+    int num = rand.nextInt(4);
+
     @Override
     public void makeNoise() {
         System.out.println(this.getName() + ", the " + this.getClass().getSimpleName().toLowerCase() + ", barked.");
+    }
+
+    @Override
+    public void roam(){
+       if(num == 0){
+           System.out.println(this.getName() + ", the " + this.getClass().getSimpleName().toLowerCase() + ", starts digging.");
+       } else{
+           System.out.println(this.getName() + ", the " + this.getClass().getSimpleName().toLowerCase() + ", runs around.");
+       }
     }
 }
