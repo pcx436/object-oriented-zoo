14bb46a53c3c46c71b7ca85f25d92dbafd31cd5e
Vin-Cento <vincentliu362@gmail.com>
2020-09-14 20:47:50 -0600

    made makeNoise and roam abstract classes

--- src/main/java/animals/felines/Feline.java
+++ src/main/java/animals/felines/Feline.java
@@ -9,23 +9,8 @@ public abstract class Feline extends Animal {
     }
 
     // abstract methods
-    public void wakeUp(){
-        System.out.println(this.getName() + ", the " + this.getClass().getSimpleName().toLowerCase() + ", woke up.");
-    }
-
-    public void sleep(){
-        System.out.println(this.getName() + ", the " + this.getClass().getSimpleName().toLowerCase() + ", went to sleep.");
-    }
-
-    public void eat(){
-        System.out.println(this.getName() + ", the " + this.getClass().getSimpleName().toLowerCase() + ", was fed.");
-    }
 
     // abstract classes
     abstract public void makeNoise();
     abstract public void roam();
-    public void eat();
-    public void roam();
-    public void sleep();
-*/
 }
