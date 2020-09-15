58f10442c99573169e9bb1f13ceaf9093821bb45
Jacob Malcy <pcx436@yahoo.com>
2020-09-15 10:38:34 -0600

    Changing argument declaration style

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import animals.Animal;
 import  animals.felines.Cat;
 
 public class Main {
-    public static void  main(String args[]){
+    public static void  main(String[] args) {
         Cat fred=new Cat("Fred",8);
         fred.wakeUp();
         fred.roam();
