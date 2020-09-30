b09d7e6888cde8b190dfeff6e210b7e2ec285c1d
Vin-Cento <vincentliu362@gmail.com>
2020-09-30 11:47:25 -0600

    commenting rest of animals

--- src/main/java/animals/pachyderm/Elephant.java
+++ src/main/java/animals/pachyderm/Elephant.java
@@ -3,10 +3,10 @@ package animals.pachyderm;
 import animals.RoamBehavior;
 
 public class Elephant extends Pachyderm{
+    // Constructor with Delegation of roam
     public Elephant(String name, int age) {
         super(name, age);
     }
-
     public Elephant(String name, int age, RoamBehavior roamBehavior) {
         super(name, age, roamBehavior);
     }
--- src/main/java/animals/pachyderm/Pachyderm.java
+++ src/main/java/animals/pachyderm/Pachyderm.java
@@ -4,16 +4,15 @@ import animals.Animal;
 import animals.RoamBehavior;
 
 public abstract class Pachyderm extends Animal{
-
+    // Constructor with Delegation of roam
     public Pachyderm(String name, int age) {
         super(name, age);
+    }
+    public Pachyderm(String name, int age, RoamBehavior roamBehavior) {
+        super(name, age, roamBehavior);
     }
 
     public void makeNoise(){
         System.out.println(this.getName() + ", the " + this.getClass().getSimpleName().toLowerCase() + ", grunted.");
-    }
-
-    public Pachyderm(String name, int age, RoamBehavior roamBehavior) {
-        super(name, age, roamBehavior);
     }
 }
