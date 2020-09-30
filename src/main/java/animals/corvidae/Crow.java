1e76382f9768cd240106eea25c0d7601689c596a
Jacob Malcy <pcx436@yahoo.com>
2020-09-30 10:19:18 -0600

    WIP: Crow constructor

--- src/main/java/animals/corvidae/Crow.java
+++ src/main/java/animals/corvidae/Crow.java
@@ -1,8 +1,14 @@
 package animals.corvidae;
 
+import animals.RoamBehavior;
+
 public class Crow extends Corvidae{
     public Crow(String name, int age) {
         super(name, age);
+    }
+
+    public Crow(String name, int age, RoamBehavior roamBehavior) {
+        super(name, age, roamBehavior);
     }
 
     @Override
