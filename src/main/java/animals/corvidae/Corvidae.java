9bccb8c85de1b26bdef27077b78a9a47bc1afbdf
Vin-Cento <vincentliu362@gmail.com>
2020-09-30 11:45:02 -0600

    commenting corvidae

--- src/main/java/animals/corvidae/Corvidae.java
+++ src/main/java/animals/corvidae/Corvidae.java
@@ -4,11 +4,10 @@ import animals.Animal;
 import animals.RoamBehavior;
 
 public abstract class Corvidae extends Animal {
-
+    // Constructor with Delegation of roam
     public Corvidae(String name, int age) {
         super(name, age);
     }
-
     public void makeNoise(){
         System.out.println(this.getName() + ", the " + this.getClass().getSimpleName().toLowerCase() + ", chirped.");
     }
