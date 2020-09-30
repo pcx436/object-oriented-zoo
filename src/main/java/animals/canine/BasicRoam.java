748c8b646462dfe1cbca4dc30e9b03d05c0d7740
Jacob Malcy <pcx436@yahoo.com>
2020-09-29 19:19:29 -0600

    Adding basic roam

--- src/main/java/animals/canine/BasicRoam.java
+++ src/main/java/animals/canine/BasicRoam.java
@@ -1,2 +1,11 @@
-package animals.canine;public class BasicRoam {
+package animals.canine;
+
+import animals.Animal;
+
+public class BasicRoam implements RoamBehavior {
+    @Override
+    public void roam(Animal animalObject) {
+        System.out.println(animalObject.getName() + ", the " + animalObject.getClass().getSimpleName().toLowerCase() +
+                ", walked around.");
+    }
 }
