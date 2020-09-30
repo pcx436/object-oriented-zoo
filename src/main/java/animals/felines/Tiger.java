92e6b0343c08a10b9e040635bc46a6c96b6fd652
Jacob Malcy <pcx436@yahoo.com>
2020-09-30 10:18:51 -0600

    WIP: Tiger constructor

--- src/main/java/animals/felines/Tiger.java
+++ src/main/java/animals/felines/Tiger.java
@@ -4,4 +4,8 @@ public class Tiger extends Feline {
     public Tiger(String name, int age) {
         super(name, age);
     }
+
+    public Tiger(String name, int age, FelineRoam roamBehavior) {
+        super(name, age, roamBehavior);
+    }
 }
