6ebd3ad346b1b80ef18c0490c6daa6cadc7b4781
Vin-Cento <vincentliu362@gmail.com>
2020-09-30 11:30:40 -0600

    reorganized and commenting ZooKeeper

--- src/main/java/zooEmployees/ZooKeeper.java
+++ src/main/java/zooEmployees/ZooKeeper.java
@@ -2,6 +2,7 @@ package zooEmployees;
 
 import animals.Animal;
 
+// SOURCE: https://www.baeldung.com/java-observer-pattern
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
@@ -9,58 +10,58 @@ import java.util.Collection;
 
 public class ZooKeeper extends ZooEmployees implements PropertyChangeListener {
     private final Collection<Animal> zooAnimals;
-    private final PropertyChangeSupport support;
-    private String state;
 
-    public void addPCL(PropertyChangeListener pcl) { support.addPropertyChangeListener(pcl); }
-    public void removePCL(PropertyChangeListener pcl) { support.removePropertyChangeListener(pcl); }
-
-    public void setState(String state) {
-        String newState = "The ZooKeeper is " + state;
-        support.firePropertyChange("state", this.state, newState);
-        this.state = newState;
-    }
-
+    // Constructor
     public ZooKeeper(String name, int age, Collection<Animal> animals) {
         super(name, age);
         this.zooAnimals = animals;
         support = new PropertyChangeSupport(this);
     }
 
+    // Methods
     public void wakeUpAnimal() {
         this.setState("waking the animals.");
         for(Animal creature : zooAnimals){
             creature.wakeUp();
         }
     }
-
     public void makeNoiseAnimal() {
         this.setState("noising the animals.");
         for(Animal creature : zooAnimals){
             creature.makeNoise();
         }
     }
-
     public void eatAnimal() {
         this.setState("feeding the animals.");
         for(Animal creature : zooAnimals){
             creature.eat();
         }
     }
-
     public void roamAnimal() {
         this.setState("walking the animals.");
         for(Animal creature : zooAnimals){
             creature.roam();
         }
     }
-
     public void sleepAnimal() {
         this.setState("put the animals to sleep.");
-
         for(Animal creature : zooAnimals){
             creature.sleep();
         }
+    }
+
+    // implementing Observable from Observer Pattern
+    private final PropertyChangeSupport support;
+    public void addPCL(PropertyChangeListener pcl) { support.addPropertyChangeListener(pcl); }
+    public void removePCL(PropertyChangeListener pcl) { support.removePropertyChangeListener(pcl); }
+
+    // implementing Observer from Observer Pattern
+    private String state;
+
+    public void setState(String state) {
+        String newState = "The ZooKeeper is " + state;
+        support.firePropertyChange("state", this.state, newState);
+        this.state = newState;
     }
 
     @Override
