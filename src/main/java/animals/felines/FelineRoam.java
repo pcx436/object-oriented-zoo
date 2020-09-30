f513480230f9c0606b6f6875131df75b5cad4d21
Vin-Cento <vincentliu362@gmail.com>
2020-09-30 11:46:29 -0600

    comment felineroam

--- src/main/java/animals/felines/FelineRoam.java
+++ src/main/java/animals/felines/FelineRoam.java
@@ -3,6 +3,7 @@ package animals.felines;
 import animals.Animal;
 import animals.RoamBehavior;
 
+// implementing roam for Feline
 public class FelineRoam implements RoamBehavior {
     @Override
     public void roam(Animal animalObj){
